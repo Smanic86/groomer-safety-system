536424d26427805eebedebaae012a830d185e6a1
Shaun <shaunhancock86@gmail.com>
2026-09-10 14:44:24 +0100

    Fix top-level middleware wrapper import

--- web-portal/src/middleware.ts
+++ web-portal/src/middleware.ts
@@ -1,8 +1,8 @@
 import { type NextRequest } from 'next/server';
-import { updateSession } from './src/lib/supabase/middleware';
+import { middleware as authMiddleware } from '@/lib/supabase/middleware';
 
 export async function middleware(request: NextRequest) {
-  return await updateSession(request);
+  return await authMiddleware(request);
 }
 
 export const config = {
